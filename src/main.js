ceb744b392f0693567370cdf57e031c126a33678
Ann <annashkurat@me.com>
2024-11-24 12:58:21 +0200

    all done

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './js/toggle-theme.js';
 import './js/projects.js';
 import './js/faq.js';
-// import './js/reviews-api.js';
-// import './js/reviews-markup.js';
-// import './js/reviews-swiper.js';
+import './js/reviews-api.js';
+import './js/reviews-markup.js';
+import './js/reviews-swiper.js';
