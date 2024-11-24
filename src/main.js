3a3c03110728c4f7b7fd76728e33f4d8806d9e40
Anna Shkurat <167918031+annashkurat1@users.noreply.github.com>
2024-11-24 12:42:44 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import * as toggles from './js/toggle-theme.js';
 import './js/reviews-api.js';
 import './js/reviews-markup.js';
 import './js/reviews-swiper.js';
+import * as toggles from './js/toggle-theme.js';
+import './js/faq.js';
